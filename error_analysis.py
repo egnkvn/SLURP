63f993b8f832d5484226fda4381eb93261f4e926
Yo-Hsin <b09902029@ntu.edu.tw>
2023-05-07 20:39:24 +0800

    error analysis (include golden text)

--- error_analysis.py
+++ error_analysis.py
@@ -3,13 +3,14 @@ import numpy as np
 import os
 import pandas as pd
 from argparse import ArgumentParser
-from matplotlib_venn import venn2
+from matplotlib_venn import venn2, venn3
 from matplotlib import pyplot as plt
 
 def parse_arguments():
     parser = ArgumentParser()
     parser.add_argument('--e2e_path', type=str, default='./e2e_method/datamap_labels.json')
     parser.add_argument('--pipeline_path', type=str, default='./pipline_method/datamap_labels.json')
+    parser.add_argument('--golden_text_path', type=str, default='./pipline_method/golden_datamap_labels.json')
     parser.add_argument('--save_path', type=str, default='./error_analysis.json')
     args = parser.parse_args()
     return args
@@ -17,31 +18,40 @@ def parse_arguments():
 def main(args):
     e2e_json = pd.read_json(args.e2e_path)
     pipeline_json = pd.read_json(args.pipeline_path)
+    golden_text_json = pd.read_json(args.golden_text_path)
     
     # Hard examples in E2E method might due to label error
     # Hard examples in two-stage method might be label error and ASR-error
     e2e_hard = set(e2e_json['hard'])
     pipeline_hard = set(pipeline_json['hard'])
+    golden_text_hard = set(golden_text_json['hard'])
     print(f'Number of hard examples in E2E method: {len(e2e_json["hard"])}')
     print(f'Number of hard examples in pipeline method: {len(pipeline_json["hard"])}')
 
-    label_err = e2e_hard
-    asr_err = pipeline_hard.difference(e2e_hard)
+    purple = e2e_hard.intersection(pipeline_hard)
+    green = pipeline_hard.intersection(golden_text_hard)
+    orange = golden_text_hard.intersection(e2e_hard)
 
-    err = {
-        'label_err': list(label_err),
-        'asr_err': list(asr_err)
-    }
+    err = dict()
+    err['pink'] = list(e2e_hard.intersection(pipeline_hard, golden_text_hard))
+    err['purple'] = list(purple.difference(err['pink']))
+    err['green'] = list(green.difference(err['pink']))
+    err['orange'] = list(orange.difference(err['pink']))
+    err['red'] = list(e2e_hard.difference(purple, orange))
+    err['yellow'] = list(golden_text_hard.difference(orange, green))
+    err['blue'] = list(pipeline_hard.difference(green, purple))
+
+    for k, v in err.items():
+        print(f'key = {k}, length = {len(v)}')
+
+    
     with open(args.save_path, 'w') as out_file:
         out_file.write(json.dumps(err, indent=4))
 
-    print(f'Number of examples with label error: {len(err["label_err"])}')
-    print(f'Number of examples with ASR error: {len(err["asr_err"])}')
-
-    venn2(
-        subsets=[e2e_hard, pipeline_hard],
-        set_labels=['E2E', 'Pipeline'],
-        set_colors=['red', 'blue']
+    venn3(
+        subsets=[e2e_hard, pipeline_hard, golden_text_hard],
+        set_labels=['E2E', 'Pipeline (Google-ASR)', 'Pipeline (golden text)'],
+        set_colors=['red', 'blue', 'yellow']
     )
     plt.savefig('./error_venn.png')
     
