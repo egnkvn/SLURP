e9b4b767039aa1e9691e089528adaab6a230fbe6
Yo-Hsin <b09902029@ntu.edu.tw>
2023-05-21 17:37:52 +0800

    error analysis (seq)

--- error_analysis.py
+++ error_analysis.py
@@ -9,8 +9,8 @@ from matplotlib import pyplot as plt
 def parse_arguments():
     parser = ArgumentParser()
     parser.add_argument('--e2e_path', type=str, default='./e2e_method/datamap_labels.json')
-    parser.add_argument('--pipeline_path', type=str, default='./pipline_method/datamap_labels.json')
-    parser.add_argument('--golden_text_path', type=str, default='./pipline_method/golden_datamap_labels.json')
+    parser.add_argument('--pipeline_path', type=str, default='./pipline_method/training_dynamic/google_analysis.json')
+    parser.add_argument('--golden_text_path', type=str, default='./pipline_method/training_dynamic/google_golden_analysis.json')
     parser.add_argument('--save_path', type=str, default='./error_analysis.json')
     args = parser.parse_args()
     return args
@@ -18,7 +18,8 @@ def parse_arguments():
 def main(args):
     e2e_json = pd.read_json(args.e2e_path)
     pipeline_json = pd.read_json(args.pipeline_path)
-    golden_text_json = pd.read_json(args.golden_text_path)
+    with open(args.golden_text_path) as json_file:
+        golden_text_json = json.load(json_file)
     
     # Hard examples in E2E method might due to label error
     # Hard examples in two-stage method might be label error and ASR-error
@@ -26,20 +27,21 @@ def main(args):
     pipeline_hard = set(pipeline_json['hard'])
     golden_text_hard = set(golden_text_json['hard'])
     print(f'Number of hard examples in E2E method: {len(e2e_json["hard"])}')
-    print(f'Number of hard examples in pipeline method: {len(pipeline_json["hard"])}')
+    print(f'Number of hard examples in pipeline method (ASR): {len(pipeline_json["hard"])}')
+    print(f'Number of hard examples in pipeline method (golden text): {len(pipeline_json["hard"])}')
 
     purple = e2e_hard.intersection(pipeline_hard)
     green = pipeline_hard.intersection(golden_text_hard)
     orange = golden_text_hard.intersection(e2e_hard)
 
     err = dict()
-    err['pink'] = list(e2e_hard.intersection(pipeline_hard, golden_text_hard))
-    err['purple'] = list(purple.difference(err['pink']))
-    err['green'] = list(green.difference(err['pink']))
-    err['orange'] = list(orange.difference(err['pink']))
-    err['red'] = list(e2e_hard.difference(purple, orange))
-    err['yellow'] = list(golden_text_hard.difference(orange, green))
-    err['blue'] = list(pipeline_hard.difference(green, purple))
+    err['pink'] = list(e2e_hard.intersection(pipeline_hard, golden_text_hard))  # E2E, ASR, Golden
+    err['purple'] = list(purple.difference(err['pink']))                        # E2E, ASR
+    err['green'] = list(green.difference(err['pink']))                          #      ASR, Golden
+    err['orange'] = list(orange.difference(err['pink']))                        # E2E,      Golden
+    err['red'] = list(e2e_hard.difference(purple, orange))                      # E2E
+    err['yellow'] = list(golden_text_hard.difference(orange, green))            #           Golden
+    err['blue'] = list(pipeline_hard.difference(green, purple))                 #       ASR
 
     for k, v in err.items():
         print(f'key = {k}, length = {len(v)}')
