c0e917584fa97e9d892225941d91e1c19af1b295
b09902021 <b09902021@meow2.csie.ntu.edu.tw>
2023-06-04 16:41:43 +0800

    merge

--- pipline_method/plot.py
+++ pipline_method/plot.py
@@ -7,9 +7,9 @@ from argparse import ArgumentParser
 
 def parse_arguments():
     parser = ArgumentParser()
-    parser.add_argument('--data_path', type=str, default='./training_dynamic/google.json')
-    parser.add_argument('--save_path', type=str, default='./google.png')
-    parser.add_argument('--title', type=str, default='Pipeline Method (Google)')
+    parser.add_argument('--data_path', type=str, default='./training_dynamic/golden.json')
+    parser.add_argument('--save_path', type=str, default='./golden.png')
+    parser.add_argument('--title', type=str, default='Pipeline Method (golden)')
     args = parser.parse_args()
     return args
 
@@ -17,12 +17,18 @@ def main(args):
     with open(args.data_path) as json_file:
         data = json.load(json_file)
     
+    datamap = {
+        'confidence': [data[fname]['prob_mean'] for fname in data.keys()],
+        'variability': [data[fname]['prob_std'] for fname in data.keys()],
+        'correct': [data[fname]['correct_mean'] for fname in data.keys()]
+    }
+    datamap = pd.DataFrame(datamap)
+
     sns.set()
-    datamap = pd.DataFrame(data)
-    pal = sns.diverging_palette(260, 15, n=len(datamap["correct_means"].unique().tolist()), sep=10, center="dark")
+    pal = sns.diverging_palette(260, 15, n=len(datamap["correct"].unique().tolist()), sep=10, center="dark")
 
     fig = plt.figure(figsize=(14, 10), )
-    sns.scatterplot(x="gold_prob_stds", y="gold_prob_means", data=datamap, palette=pal, hue="correct_means", s=10)
+    sns.scatterplot(x="variability", y="confidence", data=datamap, palette=pal, hue="correct", s=10)
 
     plt.title(f'{args.title}')
     plt.savefig(f'{args.save_path}')
